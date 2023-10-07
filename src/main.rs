8e362a3436789688f1743ba9b232f2d9b6440c49
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-10-07 19:46:53 -0400

    feat: multiple ports

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
 use std::net::TcpListener;
+
 use clap::Parser;
-
-
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
 
-    #[arg(short, long)]
-    port: u16,
-
+    #[arg(num_args(0..), short, long)]
+    ports: Vec<u16>,
 }
-
 
 
 fn main() {
     let args = Args::parse();
-    let port = args.port;
+    let ports = args.ports;
 
-
-    let is_available = port_is_available(port);
-
-    if  is_available {
-        println!("Port: {} is available", port);
-    } else {
-        println!("Port: {} is NOT available", port)
-    }
+    ports_available(ports.to_vec());
 
 }
 
+fn ports_available(ports: Vec<u16>) {
+    for port in ports {
+        if port_is_available(port) {
+            println!("port {} is available", port);
+        } else {
+            println!("Port: {} is NOT available", port)
+        }
+    }
+}
 
 
 fn port_is_available(port: u16) -> bool {
