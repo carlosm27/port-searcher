e026f5a7b84f162082a9a869125ff3aa517e15d9
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-10-07 20:32:12 -0400

    Add argument to read multiple ports

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::net::TcpListener;
 
 use clap::Parser;
 
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-
+  
     #[arg(num_args(0..), short, long)]
     ports: Vec<u16>,
+    
 }
 
 
@@ -15,19 +17,20 @@ fn main() {
     let args = Args::parse();
     let ports = args.ports;
 
-    ports_available(ports.to_vec());
+    ports_available(ports.to_vec())
 
 }
 
 fn ports_available(ports: Vec<u16>) {
     for port in ports {
         if port_is_available(port) {
-            println!("port {} is available", port);
+            println!("port: {} is available", port);
         } else {
             println!("Port: {} is NOT available", port)
         }
     }
 }
+
 
 
 fn port_is_available(port: u16) -> bool {
